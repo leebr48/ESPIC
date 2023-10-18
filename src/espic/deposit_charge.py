669d7958691ee3480fa375821e48a0caba9a2f56
Patrick Kim <pk2354@princeton.edu>
2023-10-18 12:17:11 -0400

    add charge deposition for uniform 1d grid.

---
+++ src/espic/deposit_charge.py
@@ -0,0 +1,29 @@
+import numpy as np
+from make_grid import Uniform1DGrid
+
+class ChargeDeposition():
+    
+    def __init__(self,shape='zeroth',grid=Uniform1DGrid()):
+        self.shape = shape
+        self.grid = grid
+        self.delta = grid[1] - grid[0] #Assumes uniform grid. Fix later for arbitrary grid
+        
+    def deposit(self, qarr, xarr):
+        rho = np.zeros(len(self.grid))
+        
+        for i in range(len(rho)):
+            for j in range(len(xarr)):
+                rho[i] += self.shape_func(xarr[j],self.grid[i],self.delta) * qarr[j]
+                
+        return rho
+                
+    def choose_shape(self):
+        if self.shape == 'zeroth':
+            self.shape_func = self.zeroth
+    
+    def zeroth(self, Xi, xj, delta):
+        if np.abs(xj - Xi) < delta/2:
+            return 1/delta
+        else:
+            return 0
+    
---
+++ src/espic/make_grid.py
@@ -0,0 +1,1 @@
+import numpy as npclass Uniform1DGrid():        def __init__(self, num_points=100,x0=-1,xm=1):        self.num_points = num_points        self.x0 = x0        self.xm = xm                self.grid = np.linspace(self.x0,self.xm,self.num_points)        
