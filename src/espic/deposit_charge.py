664faec7057ca0ac73730aa8d570d8cb9e618b73
Brandon Lee <leebr48@gmail.com>
2023-10-23 15:25:06 -0400

    Experimental commit refactoring weight functions

--- src/espic/deposit_charge.py
+++ src/espic/deposit_charge.py
@@ -2,11 +2,12 @@
 
 import numpy as np
 from make_grid import Uniform1DGrid
+from make_weight_funcs import ChargeWeightFunc
 
 
 class ChargeDeposition:
-    def __init__(self, shape="zeroth", grid=Uniform1DGrid()):
-        self.shape = shape
+    def __init__(self, shape_func="zeroth_order", grid=Uniform1DGrid()):
+        self.shape_func = ChargeWeightFunc.shape_func
         self.grid = grid
         self.delta = (
             grid[1] - grid[0]
@@ -15,15 +16,11 @@ class ChargeDeposition:
     def deposit(self, qarr, xarr):
         rho = np.zeros(len(self.grid))
 
-        for i in range(len(rho)):
+        for i in range(len(rho)): #FIXME should be vectorized with numpy if possible, possibly in ChargeWeightFunc
             for j in range(len(xarr)):
                 rho[i] += self.shape_func(xarr[j], self.grid[i], self.delta) * qarr[j]
 
         return rho
-
-    def choose_shape(self):
-        if self.shape == "zeroth":
-            self.shape_func = self.zeroth
 
     def zeroth(self, Xi, xj, delta):
         if np.abs(xj - Xi) < delta / 2:
---
+++ src/espic/make_weight_func.py
@@ -0,0 +1,17 @@
+"""Create weight functions for charge and electric field.""" # FIXME add classes to comment.
+
+import numpy as np
+
+
+def ChargeWeightFunc:
+    
+    def __init__(self, evalutation_point, grid_point, spatial_grid_delta):
+        self.evalutation_point = Xi
+        self.grid_point = grid_point
+        self.spatial_grid_delta = spatial_grid_delta
+
+    def zeroth_order(self):
+        if np.abs(self.grid_point - self.evalutation_point) < self.spatial_grid_delta / 2:
+            return 1 / self.spatial_grid_delta
+        else:
+            return 0
