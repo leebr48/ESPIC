5715e4c279244eee8e8ce9dbf7e09cd922f5bc61
Patrick Kim <pk2354@princeton.edu>
2023-10-23 21:48:35 -0400

    fully implemented 1d maxwell solver.

--- src/espic/__init__.py
+++ src/espic/__init__.py
@@ -1,1 +1,4 @@
 """File is necessary for Python to recognize ESPIC as a package."""
+from make_grid import Uniform1DGrid
+from solve_maxwell import MaxwellSolver1D
+from deposit_charge import ChargeDeposition
--- src/espic/solve_maxwell.py
+++ src/espic/solve_maxwell.py
@@ -5,22 +5,30 @@ from scipy.linalg import solve_banded
 
 class MaxwellSolver1D:
     def __init__(self, grid=Uniform1DGrid(), boundary_conditions=np.zeros(2)):
-        self.grid = grid
+        self.grid = grid.grid
         self.boundary_conditions = boundary_conditions
         self.phi = np.zeros(len(self.grid))
 
     # Centered differences. Should we make it arbitrary?
     def solve(self, rho):
         delta = self.grid[1] - self.grid[0]
-        dim = len(self.grid)
+        dim = len(self.grid) - 2
+        phi = np.zeros(len(self.grid))
+        phi[0] = self.boundary_conditions[0]
+        phi[-1] = self.boundary_conditions[1]
+        
         bands = np.empty((3, dim))
         bands[0, 1:] = np.ones(dim - 1)
         bands[1, :] = -2 * np.ones(dim)
         bands[2, :-1] = np.ones(dim - 1)
-
+        
+        rho = rho[1:-1]
         bc = np.zeros(dim)
         bc[0] = self.boundary_conditions[0]
         bc[-1] = self.boundary_conditions[1]
         rhs = -4 * np.pi * delta**2 * rho + bc
+        
+        phi[1:-1] = solve_banded((1, 1), bands, rhs)
 
-        return solve_banded((1, 1), bands, rhs)
+        return phi
+    
