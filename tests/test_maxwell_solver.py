ee5ac0e8ed8d0a24640939c3a6226b5ba14a38e7
Brandon Lee <leebr48@gmail.com>
2023-10-23 22:42:40 -0400

    Cleaned __init__.py, fixed import for MaxwellSolver1D in tests, and added some linting fixes

--- tests/test_maxwell_solver.py
+++ tests/test_maxwell_solver.py
@@ -1,12 +1,14 @@
 import numpy as np
-from solve_maxwell import MaxwellSolver1D
 
-#Test 1D Maxwell solver for a uniform charge distribution
+from espic.solve_maxwell import MaxwellSolver1D
+
+
 def test_1d_uniform():
+    """Test 1D Maxwell solver for a uniform charge distribution"""
     ms = MaxwellSolver1D()
     rho = np.ones(len(ms.grid))
     phi = ms.solve(rho)
 
-    true_phi = -2*np.pi*(ms.grid**2 - 1)
+    true_phi = -2 * np.pi * (ms.grid**2 - 1)
     err = np.abs(true_phi - phi)
     np.testing.assert_allclose(err, np.zeros(len(err)), atol=1e-9)
