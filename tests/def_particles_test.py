e3a3321f6e34804d25ecf492fb59c3d49f6b132b
leebr48 <31461969+leebr48@users.noreply.github.com>
2023-11-14 23:14:05 -0500

    Particle pusher (#3)
    * Initial particle push commit, nothing is complete
    
    * Updates to particle pusher, still needs more work
    
    * Fleshed out particle pusher
    
    * Ran linters

---
+++ tests/def_particles_test.py
@@ -0,0 +1,31 @@
+"""Test particle class"""
+
+import numpy as np
+
+from espic.def_particles import Particles
+
+
+def test_particles_1D():
+    qs = np.asarray([1, 2])
+    ms = np.asarray([5.5, 6.5])
+    positions = np.asarray([[1.1], [2.2]])
+    velocities = np.asarray([[-2.5], [-3.5]])
+    particles = Particles(qs, ms, positions, velocities)
+
+    assert np.array_equal(particles.charges, qs)
+    assert np.array_equal(particles.masses, ms)
+    assert np.array_equal(particles.positions, positions)
+    assert np.array_equal(particles.velocities, velocities)
+
+
+def test_particle_2D():
+    qs = np.asarray([1, 2])
+    ms = np.asarray([5.5, 6.5])
+    positions = np.asarray([[1.1, -2.2], [-3.3, 4.4]])
+    velocities = np.asarray([[-2.5, 3.3], [4.7, -5.5]])
+    particles = Particles(qs, ms, positions, velocities)
+
+    assert np.array_equal(particles.charges, qs)
+    assert np.array_equal(particles.masses, ms)
+    assert np.array_equal(particles.positions, positions)
+    assert np.array_equal(particles.velocities, velocities)
